539478bcf93e64f9c321e7f4ada6707d7c5efbd2
thejsj <jorge.silva@thejsj.com>
2016-08-03 13:32:20 -0700

    Add tests

---
+++ test/fixtures/stripe-events.js
@@ -0,0 +1,62 @@
+'use strict'
+
+/**
+ * Mock github event request options.
+ * @type {Object}
+ */
+module.exports = {
+  malformed: {
+    "object": "event",
+    "api_version": "2015-10-12",
+    "created": 1470185308,
+    "data": {},
+    "livemode": false,
+    "pending_webhooks": 0,
+    "request": "req_8w5kR3LRuAl6hU",
+    "type": "customer.subscription.created"
+  },
+  unhandled: {
+    "id": "evt_18eDYeLYrJgOrBWzMApFSM7T",
+    "object": "event",
+    "api_version": "2015-10-12",
+    "created": 1470185308,
+    "data": {},
+    "livemode": false,
+    "pending_webhooks": 0,
+    "request": "req_8w5kR3LRuAl6hU",
+    "type": "customer.subscription.created"
+  },
+  "invoice.created": {
+    "id": "evt_18eDYeLYrJgOrBWzMApFSM11",
+    "object": "event",
+    "api_version": "2015-10-12",
+    "created": 1470185308,
+    "data": {},
+    "livemode": false,
+    "pending_webhooks": 0,
+    "request": "req_8w5kR3LRuAl6hU",
+    "type": "invoice.created"
+  },
+  "invoice.payment_succeeded": {
+    "id": "evt_18eDYeLYrJgOrBWzMApFSM22",
+    "object": "event",
+    "api_version": "2015-10-12",
+    "created": 1470185308,
+    "data": {},
+    "livemode": false,
+    "pending_webhooks": 0,
+    "request": "req_8w5kR3LRuAl6hU",
+    "type": "invoice.payment_succeeded"
+  },
+  "invoice.payment_failed": {
+    "id": "evt_18eDYeLYrJgOrBWzMApFSM33",
+    "object": "event",
+    "api_version": "2015-10-12",
+    "created": 1470185308,
+    "data": {},
+    "livemode": false,
+    "pending_webhooks": 0,
+    "request": "req_8w5kR3LRuAl6hU",
+    "type": "invoice.payment_failed"
+  }
+}
--- test/functional/github.js
+++ test/functional/github.js
@@ -19,133 +19,131 @@ const httpServer = require('../../lib/http/server')
 const rabbitmq = require('../../lib/rabbitmq')
 const workerServer = require('../../lib/worker/server')
 
-describe('functional', () => {
-  const webhookUrl = `http://localhost:${process.env.PORT}/github`
+const webhookUrl = `http://localhost:${process.env.PORT}/github`
 
-  describe('Github Webhook', () => {
-    before(() => {
-      return rabbitmq.connect()
-        .then(() => { return workerServer.start() })
-        .then(() => { return httpServer.start() })
-        .then(() => { return api.startAsync() })
-    })
+describe('Github Webhook Functional', () => {
+  before(() => {
+    return rabbitmq.connect()
+      .then(() => { return workerServer.start() })
+      .then(() => { return httpServer.start() })
+      .then(() => { return api.startAsync() })
+  })
 
-    after(() => {
-      return api.stopAsync()
-        .then(() => { return httpServer.stop() })
-        .then(() => { return workerServer.stop() })
-        .then(() => { return rabbitmq.disconnect() })
-    })
+  after(() => {
+    return api.stopAsync()
+      .then(() => { return httpServer.stop() })
+      .then(() => { return workerServer.stop() })
+      .then(() => { return rabbitmq.disconnect() })
+  })
 
-    beforeEach(() => {
-      api.stub('POST', '/actions/github').returns(202)
-    })
+  beforeEach(() => {
+    api.stub('POST', '/actions/github').returns(202)
+  })
 
-    afterEach(() => {
-      api.restore()
-    })
+  afterEach(() => {
+    api.restore()
+  })
 
-    it('should respond to push events then enqueue and handle the job', () => {
-      const push = request.postAsync(webhookUrl, events.push)
-      return assert.isFulfilled(push)
-        .then((data) => {
-          assert.equal(data.statusCode, 201)
-        })
-        .then(() => { return Promise.delay(300) })
-        .then(() => {
-          sinon.assert.calledOnce(api.stub('POST', '/actions/github'))
-        })
-    })
-
-    it('should retry until the job passes', () => {
-      const stub = api.stub('POST', '/actions/github')
-      stub.onFirstCall().returns(500)
-        .onSecondCall().returns(500)
-      const push = request.postAsync(webhookUrl, events.push)
-      return assert.isFulfilled(push)
-        .then(() => { return Promise.delay(300) })
-        .then(() => {
-          assert.equal(stub.callCount, 3)
-        })
-    })
-
-    it('should not report on a "not a runnable user" error', () => {
-      sinon.stub(workerServer.errorCat, 'report')
-      const stub = api.stub('POST', '/actions/github').returns({
-        status: 400,
-        body: 'Commit author/committer is not a Runnable user'
+  it('should respond to push events then enqueue and handle the job', () => {
+    const push = request.postAsync(webhookUrl, events.push)
+    return assert.isFulfilled(push)
+      .then((data) => {
+        assert.equal(data.statusCode, 201)
       })
-      const push = request.postAsync(webhookUrl, events.push)
-      return assert.isFulfilled(push)
-        .then(() => { return Promise.delay(300) })
-        .then(() => {
-          sinon.assert.calledOnce(stub)
-          assert.equal(workerServer.errorCat.report.callCount, 0)
-        })
-        .finally(() => {
-          workerServer.errorCat.report.restore()
-        })
-    })
+      .then(() => { return Promise.delay(300) })
+      .then(() => {
+        sinon.assert.calledOnce(api.stub('POST', '/actions/github'))
+      })
+  })
 
-    it('should not retry on a redirect or client error from api', () => {
-      const stub = api.stub('POST', '/actions/github').returns(300)
-      const push = request.postAsync(webhookUrl, events.push)
-      return assert.isFulfilled(push)
-        .then(() => { return Promise.delay(300) })
-        .then(() => {
-          sinon.assert.calledOnce(stub)
-        })
-    })
+  it('should retry until the job passes', () => {
+    const stub = api.stub('POST', '/actions/github')
+    stub.onFirstCall().returns(500)
+      .onSecondCall().returns(500)
+    const push = request.postAsync(webhookUrl, events.push)
+    return assert.isFulfilled(push)
+      .then(() => { return Promise.delay(300) })
+      .then(() => {
+        assert.equal(stub.callCount, 3)
+      })
+  })
 
-    it('should respond to ping events', () => {
-      const ping = request.postAsync(webhookUrl, events.ping)
-      return assert.isFulfilled(ping)
-        .then((data) => {
-          assert.equal(data.statusCode, 200)
-          assert.equal(data.body, 'pong')
-        })
+  it('should not report on a "not a runnable user" error', () => {
+    sinon.stub(workerServer.errorCat, 'report')
+    const stub = api.stub('POST', '/actions/github').returns({
+      status: 400,
+      body: 'Commit author/committer is not a Runnable user'
     })
+    const push = request.postAsync(webhookUrl, events.push)
+    return assert.isFulfilled(push)
+      .then(() => { return Promise.delay(300) })
+      .then(() => {
+        sinon.assert.calledOnce(stub)
+        assert.equal(workerServer.errorCat.report.callCount, 0)
+      })
+      .finally(() => {
+        workerServer.errorCat.report.restore()
+      })
+  })
 
-    it('should ignore all other webhook events', () => {
-      const watch = request.postAsync(webhookUrl, events.watch)
-      return assert.isFulfilled(watch)
-        .then((data) => {
-          assert.equal(data.statusCode, 202)
-        })
-    })
+  it('should not retry on a redirect or client error from api', () => {
+    const stub = api.stub('POST', '/actions/github').returns(300)
+    const push = request.postAsync(webhookUrl, events.push)
+    return assert.isFulfilled(push)
+      .then(() => { return Promise.delay(300) })
+      .then(() => {
+        sinon.assert.calledOnce(stub)
+      })
+  })
 
-    it('should reject malformed web-hook requests', () => {
-      const malformed = request.postAsync(webhookUrl, events.malformed)
-      return assert.isFulfilled(malformed)
-        .then((data) => {
-          assert.equal(data.statusCode, 400)
-          assert.equal(data.body, 'Invalid Webhook Request')
-        })
-    })
+  it('should respond to ping events', () => {
+    const ping = request.postAsync(webhookUrl, events.ping)
+    return assert.isFulfilled(ping)
+      .then((data) => {
+        assert.equal(data.statusCode, 200)
+        assert.equal(data.body, 'pong')
+      })
+  })
 
-    it('should fatally reject malformed jobs', () => {
-      const stub = api.stub('POST', '/actions/github')
-      return Promise.resolve()
-        .then(() => {
-          rabbitmq.publishToQueue('github.push', { deliveryId: 'fools' })
-          return Promise.delay(500)
-        })
-        .then(() => {
-          assert.equal(stub.callCount, 0)
-        })
-    })
+  it('should ignore all other webhook events', () => {
+    const watch = request.postAsync(webhookUrl, events.watch)
+    return assert.isFulfilled(watch)
+      .then((data) => {
+        assert.equal(data.statusCode, 202)
+      })
+  })
 
-    it('should gracefully handle unexpected errors', () => {
-      sinon.stub(Joi, 'validateAsync', () => { throw Error('oopsies...') })
-      const ping = request.postAsync(webhookUrl, events.ping)
-      return assert.isFulfilled(ping)
-        .then((data) => {
-          assert.equal(data.statusCode, 500)
-          assert.equal(data.body, 'Internal Server Error')
-        })
-        .finally(() => {
-          Joi.validateAsync.restore()
-        })
-    })
-  }) // end 'Github Webhook'
-}) // end 'functional'
+  it('should reject malformed web-hook requests', () => {
+    const malformed = request.postAsync(webhookUrl, events.malformed)
+    return assert.isFulfilled(malformed)
+      .then((data) => {
+        assert.equal(data.statusCode, 400)
+        assert.equal(data.body, 'Invalid Webhook Request')
+      })
+  })
+
+  it('should fatally reject malformed jobs', () => {
+    const stub = api.stub('POST', '/actions/github')
+    return Promise.resolve()
+      .then(() => {
+        rabbitmq.publishToQueue('github.push', { deliveryId: 'fools' })
+        return Promise.delay(500)
+      })
+      .then(() => {
+        assert.equal(stub.callCount, 0)
+      })
+  })
+
+  it('should gracefully handle unexpected errors', () => {
+    sinon.stub(Joi, 'validateAsync', () => { throw Error('oopsies...') })
+    const ping = request.postAsync(webhookUrl, events.ping)
+    return assert.isFulfilled(ping)
+      .then((data) => {
+        assert.equal(data.statusCode, 500)
+        assert.equal(data.body, 'Internal Server Error')
+      })
+      .finally(() => {
+        Joi.validateAsync.restore()
+      })
+  })
+}) // end 'Github Webhook'
